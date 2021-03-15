69cc2904a5e6f5ea4dc7206809b0d6397180feb0
kassimii <patyboc@yahoo.com>
2021-03-15 21:30:48 +0200

    refactored

--- src/main.py
+++ src/main.py
@@ -4,80 +4,106 @@ properties_separator = '='
 lang_separator = ','
 accepted_langs_separator = ' ('
 project_properties = {}
-
-with open('../project.properties') as f:
-    for line in f:
-        if properties_separator in line:
-            name, value = line.split(properties_separator, 1)
-            project_properties[name.strip()] = value.strip()
-
 code_results = []
 blank_results = []
 comment_results = []
-
-project_lang = project_properties.get('project.lang')
-langs = project_lang.split(lang_separator)
-
 lowercase_langs = []
-
-for elem in langs:
-    lowercase_langs.append(elem.lower())
-
 raw_accepted_languages = []
+unaccepted_langs = []
 
-with open('../cfg/languages.cfg') as f:
-    for line in f:
-        if accepted_langs_separator in line:
-            lang, ext = line.split(accepted_langs_separator, 1)
-            raw_accepted_languages.append(lang.lower())
+
+def get_project_properties():
+    with open('../project.properties') as f:
+        for line in f:
+            if properties_separator in line:
+                name, value = line.split(properties_separator, 1)
+                project_properties[name.strip()] = value.strip()
+
+
+def lowercase_langs_letters(lang_list):
+    for elem in lang_list:
+        lowercase_langs.append(elem.lower())
+
+
+def get_accepted_langs():
+    with open('../cfg/languages.cfg') as f:
+        for line in f:
+            if accepted_langs_separator in line:
+                lang, ext = line.split(accepted_langs_separator, 1)
+                raw_accepted_languages.append(lang.lower())
+
+
+def get_raw_json(input_file):
+    with open(input_file) as json_file:
+        results = json.load(json_file)
+        return results
+
+
+def verify_accepted_langs():
+    for elem in lowercase_langs:
+        if elem not in raw_accepted_languages:
+            unaccepted_langs.append(elem)
+
+
+def modify_json_format():
+    for i in range(0, len(raw_results)):
+        for j in range(0, len(raw_results[i].get('Files'))):
+            if raw_results[i].get('Files')[j].get('Language').lower() in lowercase_langs and \
+                    raw_results[i].get('Files')[j].get('Language').lower() in raw_accepted_languages:
+                file_path = raw_results[i].get('Files')[j].get('Location').replace(project_path + '/', '')
+                code_results.append({
+                    'name': raw_results[i].get('Files')[j].get('Language'),
+                    'category': 'Code',
+                    'file': file_path,
+                    'value': raw_results[i].get('Files')[j].get('Code'),
+                })
+
+                blank_results.append({
+                    'name': raw_results[i].get('Files')[j].get('Language'),
+                    'category': 'Blank',
+                    'file': file_path,
+                    'value': raw_results[i].get('Files')[j].get('Blank'),
+                })
+
+                comment_results.append({
+                    'name': raw_results[i].get('Files')[j].get('Language'),
+                    'category': 'Comment',
+                    'file': file_path,
+                    'value': raw_results[i].get('Files')[j].get('Comment'),
+                })
+
+
+def generate_new_json(output_file, results):
+    with open(output_file, 'w') as outfile:
+        json.dump(results, outfile, indent=4)
+
+
+get_project_properties()
 
 project_name = project_properties.get('project.name')
 project_path = project_properties.get('project.path')
+project_lang = project_properties.get('project.lang')
+langs = project_lang.split(lang_separator)
+
+print(project_path)
+
+lowercase_langs_letters(langs)
+
+get_accepted_langs()
 
 input_json_file = '../tmp/' + project_name + '-raw.json'
 
-with open(input_json_file) as json_file:
-    raw_results = json.load(json_file)
+raw_results = get_raw_json(input_json_file)
 
-unaccepted_langs = []
-
-for elem in lowercase_langs:
-    if elem not in raw_accepted_languages:
-        unaccepted_langs.append(elem)
+verify_accepted_langs()
 
 if len(unaccepted_langs) > 0:
     print('The following languages are not supported:', unaccepted_langs)
 
-for i in range(0, len(raw_results)):
-    for j in range(0, len(raw_results[i].get('Files'))):
-        if raw_results[i].get('Files')[j].get('Language').lower() in lowercase_langs and raw_results[i].get('Files')[j].get('Language').lower() in raw_accepted_languages:
-            file_path = raw_results[i].get('Files')[j].get('Location').replace(project_path + '/', '')
-            code_results.append({
-                'name': raw_results[i].get('Files')[j].get('Language'),
-                'category': 'Code',
-                'file': file_path,
-                'value': raw_results[i].get('Files')[j].get('Code'),
-            })
-
-            blank_results.append({
-                'name': raw_results[i].get('Files')[j].get('Language'),
-                'category': 'Blank',
-                'file': file_path,
-                'value': raw_results[i].get('Files')[j].get('Blank'),
-            })
-
-            comment_results.append({
-                'name': raw_results[i].get('Files')[j].get('Language'),
-                'category': 'Comment',
-                'file': file_path,
-                'value': raw_results[i].get('Files')[j].get('Comment'),
-            })
+modify_json_format()
 
 dx_results = code_results + blank_results + comment_results
 
 output_json_file = '../out/' + project_name + '-results.json'
 
-with open(output_json_file, 'w') as outfile:
-    json.dump(dx_results, outfile, indent=4)
-
-
+generate_new_json(output_json_file, dx_results)
